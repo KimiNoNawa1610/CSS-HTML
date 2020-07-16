2205360ccb895c3be721ec78a9bb602508e2c909
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-15 20:51:36 -0700

    Update source.js

--- Websql/source.js
+++ Websql/source.js
@@ -1,2 +1,17 @@
 var faker=require('faker');
 var mysql=require('mysql');
+
+var mysqlConnections=mysql.createConnection({
+    host:'localhoast',
+    user:'Nhan',
+    password:'1610',
+    database:
+})
+
+function AddressGenerator(){
+    console.log(faker.address.streetAddress());
+    console.log(faker.address.city());
+    console.log(faker.address.state());
+}
+
+AddressGenerator();
