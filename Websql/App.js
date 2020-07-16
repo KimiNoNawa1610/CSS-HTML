1b96e8bcaacf267794bd5c1684a8714f1c1e8392
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-16 16:15:08 -0700

    update websql

--- Websql/App.js
+++ Websql/App.js
@@ -1,10 +1,27 @@
 var express=require('express');
 var app=express();
+var mysql = require('mysql');
 
-app.get("/", function(req,res){
-    res.send("Hello from our websql");
+app.set("view engine","ejs");
+
+var mysqlConnections = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'websql'
 });
 
-app.listen(8080,function(){
-    console.log('App is listening on port 8080');
+app.get("/", function(req,res){
+    var q='Selection count(*) as count from users';
+    mysqlConnections.query(q, function(error,results){
+        if(error)throw error;
+        var count=results[0].cont;
+        res.render("home",{data:count});
+    })
+});
+
+
+
+app.listen(3128,function(){
+    console.log('App is running on port 8080');
 });
