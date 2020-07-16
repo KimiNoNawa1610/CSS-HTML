68b25384363e6097475523ecb387e53e79035530
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-16 16:39:18 -0700

    Update App.js

--- Websql/App.js
+++ Websql/App.js
@@ -1,8 +1,11 @@
 var express=require('express');
 var app=express();
 var mysql = require('mysql');
+var bodyParser= require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+app.engine('html', require('ejs').renderFile);
 
 var mysqlConnections = mysql.createConnection({
     host: 'localhost',
@@ -16,12 +19,21 @@ app.get("/", function(req,res){
     mysqlConnections.query(q, function(error,results){
         if(error)throw error;
         var count=results[0].cont;
-        res.render("home",{data:count});
+        res.render("home", { data: count });
     })
 });
 
 
 
 app.listen(3128,function(){
-    console.log('App is running on port 8080');
+    console.log('App is running on port 3128');
+});
+
+app.post('/register', function (req, res) {
+    var person = { email: req.body.email };
+    connection.query('INSERT INTO users SET ?', person, function (err, result) {
+        console.log(err);
+        console.log(result);
+        res.redirect("/");
+    });
 });
