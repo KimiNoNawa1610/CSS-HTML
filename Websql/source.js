9d303a9b0753daf88091f8aed139ae8e09016048
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-16 14:12:05 -0700

    Update source.js

--- Websql/source.js
+++ Websql/source.js
@@ -25,10 +25,11 @@ mysqlConnections.connect(function(error){
 /* Number of Users*/
 var select ='Select count(*) as total from users ';
 
-mysqlConnections.query(select, function (error,results,fileds) {
+/*mysqlConnections.query(select, function (error,results,fileds) {
     if(error) throw error;
     console.log(results[0].total);
-});
+});*/
+
 /*Insert Data*/
 
 var insert="insert into users(email) values('Nhan@gmail.com');" 
@@ -37,8 +38,17 @@ var insert="insert into users(email) values('Nhan@gmail.com');"
     console.log(results[0].total);
 }); */
 
-var person={email:faker.internet.email()};
-mysqlConnections.query("Insert into users SET ?",person, function(error,result){
+/*var person={email:faker.internet.email()};*/
+var people=[];
+for(var i=0;i<500;i++){
+    people.push([
+        faker.internet.email(),
+        faker.date.past()
+    ]);
+}
+var q='insert into users (email, created_at) values ?';
+//console.log(people);
+mysqlConnections.query(q,[people], function(error,result){
     if(error) throw error;
     console.log(result);
 })
