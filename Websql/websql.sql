1c2c1045a49aaed76715c97005772705fae8f648
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-16 14:24:27 -0700

    Update websql.sql

--- Websql/websql.sql
+++ Websql/websql.sql
@@ -2,6 +2,8 @@ create database websql;
 
 use websql;
 
+drop table users;
+
 create table users(
 
 	email varchar(255) primary key,
@@ -10,6 +12,20 @@ create table users(
     
 );
 
-insert into users(email) values ("talon@gmail.com"),("Bee@gmail.com");
+select count(*) from users;
 
-select * from users;
+select created_at as earlies_date from users where created_at=(select min(created_at) from users);
+
+select email, created_at from users where created_at=(select min(created_at) from users);
+
+select monthname(created_at) as months, count(email) from users group by months order by months;
+
+select count(email) as yahoo_users from users where email like '%@yahoo%';
+
+select case 
+when email like'%@yahoo%' then 'yahoo_users '
+when email like '%@gmail%' then 'gmail_users '
+when email like'%@hotmail%' then ' hotmail_users'
+else 'other' end as email_providers, count(email) as total_users from users group by email_providers;
+
+
